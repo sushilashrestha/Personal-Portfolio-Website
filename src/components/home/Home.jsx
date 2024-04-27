e5fa26ffdef48fd6a8511e3065a522aa060a2609
sushilashrestha <susshhh23@gmail.com>
2024-04-27 19:58:41 +0545

    Add profile image to home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,22 +3,16 @@ import "./home.css"
 import Social from "./Social"
 import Data from "./Data"
 import ScrollDown from './ScrollDown'
-import profileImg from "../../assets/profile.png"
 
 const Home = () => {
   return (
     <section className="home section" id="home">
 
-
-
         <div className="home__container container grid">
             <div className="home__content grid ">
-            
                 <Social />
-                <img src={profileImg} alt="" className= "home__img" />
 
                 <div className="home__img"></div>
-               
 
                 <Data />
             </div>
